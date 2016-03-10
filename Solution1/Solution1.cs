97152137cafe3d86dde35eee234139a964b9d31c
romansp <romanpavlv@gmail.com>
2016-03-10 15:19:25 +0300

    Solution 1 wip

---
+++ Solution1/Solution1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+
+namespace System
+{
+	using Algorithms;
+
+	public struct Rational : IEquatable<Rational>, IComparable<Rational>, IComparable
+	{
+		private readonly int _numerator;
+		private readonly int _denominator;
+		private readonly int _gcd;
+		private readonly int _sign;
+
+		public Rational(int numerator, int denominator)
+		{
+			if (denominator == 0)
+				throw new ArgumentException("Denominator can't be equal to zero.", "denominator");
+
+			_numerator = numerator;
+			_denominator = denominator;
+			_gcd = Algorithms.Gcd(_numerator, _denominator);
+			_sign = numerator < 0 && denominator < 0 || numerator > 0 && denominator > 0 ? 1 : -1;
+		}
+
+		public int Numerator { get { return _numerator; } }
+		public int Denominator { get { return _denominator; } }
+
+		public Rational Simplify()
+		{
+			return new Rational(_numerator / _gcd, _denominator / _gcd);
+		}
+
+		public static Rational operator +(Rational r1, Rational r2)
+		{
+			checked
+			{
+				return (r1._denominator == r2._denominator
+					? new Rational(r1._numerator + r2._numerator, r1._denominator)
+					: new Rational(r1._numerator * r2._denominator + r2._numerator * r1._denominator, r1._denominator * r2._denominator)
+				).Simplify();
+			}
+		}
+
+		public static Rational operator -(Rational r1, Rational r2)
+		{
+			checked
+			{
+				return (r1._denominator == r2._denominator
+					? new Rational(r1._numerator - r2._numerator, r1._denominator)
+					: new Rational(r1._numerator * r2._denominator - r2._numerator * r1._denominator, r1._denominator * r2._denominator)
+				).Simplify();
+			}
+		}
+
+		public static Rational operator -(Rational r1)
+		{
+			checked
+			{
+				return new Rational(-r1._numerator, r1._denominator).Simplify();
+			}
+		}
+
+		public static Rational operator *(Rational r1, Rational r2)
+		{
+			checked
+			{
+				return new Rational(r1._numerator * r2._numerator, r1._denominator * r2._denominator).Simplify();
+			}
+		}
+
+		public static Rational operator /(Rational r1, Rational r2)
+		{
+			checked
+			{
+				return new Rational(r1._numerator * r2._denominator, r1._denominator / r2._numerator).Simplify();
+			}
+		}
+
+		public static bool operator ==(Rational r1, Rational r2)
+		{
+			return r1.Equals(r2);
+		}
+
+		public static bool operator !=(Rational r1, Rational r2)
+		{
+			return !r1.Equals(r2);
+		}
+
+		public static bool operator >(Rational r1, Rational r2)
+		{
+			return r1.CompareTo(r2) > 0;
+		}
+
+		public static bool operator <(Rational r1, Rational r2)
+		{
+			return r1.CompareTo(r2) < 0;
+		}
+
+		public static bool operator >=(Rational r1, Rational r2)
+		{
+			return r1.CompareTo(r2) >= 0;
+		}
+
+		public static bool operator <=(Rational r1, Rational r2)
+		{
+			return r1.CompareTo(r2) <= 0;
+		}
+
+
+		public static explicit operator double(Rational r1)
+		{
+			return (double)r1._numerator / r1._denominator;
+		}
+
+		public static implicit operator Rational(int numerator)
+		{
+			return new Rational(numerator, 1);
+		}
+
+		public int CompareTo(Rational other)
+		{
+			if (Equals(other))
+				return 0;
+			checked
+			{
+				if (_sign != other._sign)
+					return _sign;
+				
+				// store in long type to avoid overflow
+				var left = (long)_numerator * other._denominator;
+				var right = (long)other._numerator * _denominator;
+
+				//if (_sign < 0 && other._sign < 0)
+				//	return right.CompareTo(left); 
+
+				return left.CompareTo(right);
+			}
+		}
+
+		public int CompareTo(object obj)
+		{
+			if (ReferenceEquals(obj, null))
+				return 1;
+			if (!(obj is Rational)) throw new ArgumentException("Object is not a Rational", "obj");
+			return CompareTo((Rational)obj);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			return obj is Rational && Equals((Rational)obj);
+		}
+
+		public bool Equals(Rational other)
+		{
+			if (_numerator == 0)
+			{
+				return other._numerator == 0;
+			}
+			// compare simplified rationals to avoid overflow
+			return _numerator / _gcd == other._numerator / other._gcd && _denominator / _gcd == other._denominator / other._gcd;
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (_numerator / _gcd * 397) ^ (_denominator / _gcd);
+			}
+		}
+
+		public override string ToString()
+		{
+			if (_numerator == 0) return "0";
+			if (_denominator == 1) return _numerator.ToString();
+			return _numerator.ToString() + "/" + _denominator.ToString() + " " + (double)this;
+		}
+	}
+}
+
+namespace Algorithms
+{
+	public static class Algorithms
+	{
+		/// <summary>
+		/// Finds the greatest common divisor of two int values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static int Gcd(int a, int b)
+		{
+			while (b != 0)
+			{
+				var temp = b;
+				b = a % b;
+				a = temp;
+			}
+			return a;
+		}
+
+		/// <summary>
+		/// Finds the least common multiple of two int values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static int Lcm(int a, int b)
+		{
+			return a * b / Gcd(a, b);
+		}
+	}
+
+}
+
+namespace research2016
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ research2016Tests/Solution1Tests.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace research2016Tests
+{
+	public class Solution1Tests
+	{
+		[Fact]
+		public void Rational_is_in_System_namespace()
+		{
+			Assert.True(typeof(Rational).Namespace == typeof(string).Namespace);
+		}
+
+		[Fact]
+		public void Rational_is_value_type()
+		{
+			Assert.True(typeof(Rational).IsValueType);
+		}
+
+		[Theory]
+		[InlineData(0, 0)]
+		[InlineData(1, 0)]
+		[InlineData(-1, 0)]
+		public void Rational_incorrect_arguments_throws_ArgumentException(int num, int denom)
+		{
+			Assert.Throws<ArgumentException>(() => new Rational(num, denom));
+		}
+
+		[Theory]
+		[InlineData(2, 3, "2/3")]
+		[InlineData(-2, 4, "-2/4")]
+		[InlineData(1, -2, "1/-2")]
+		[InlineData(-2, -2, "-2/-2")]
+		[InlineData(0, 1, "0")]
+		[InlineData(42, 1, "42")]
+		public void Rational_ToString_formats(int num, int denom, string formatted)
+		{
+			var rational = new Rational(num, denom);
+			Assert.True(Equals(rational.ToString(), formatted));
+		}
+
+		[Theory]
+		[InlineData(-2, 4, -1, 2)]
+		[InlineData(-2, -4, 1, 2)]
+		[InlineData(2, -4, -1, 2)]
+		[InlineData(2, 4, 1, 2)]
+		[InlineData(42, 2, 21, 1)]
+		[InlineData(0, 1, 0, 1)]
+		[InlineData(int.MinValue, int.MinValue, 1, 1)]
+		[InlineData(int.MaxValue, int.MaxValue, 1, 1)]
+		[InlineData(int.MinValue, int.MaxValue, int.MinValue, int.MaxValue)]
+		[InlineData(int.MaxValue, int.MinValue, int.MaxValue, int.MinValue)]
+		public void Rational_Simplify(int num, int denom, int numSimpl, int denomSimpl)
+		{
+			var rationalSimpl = new Rational(num, denom).Simplify();
+			Assert.Equal(numSimpl, rationalSimpl.Numerator);
+			Assert.Equal(denomSimpl, rationalSimpl.Denominator);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, "1")]
+		public void Rational_Sum(int r1Num, int r1Denom, int r2Num, int r2Denom, string stringResult)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			var result = rational1 + rational2;
+
+			Assert.Equal(stringResult, result.ToString());
+		}
+
+		[Theory]
+		[InlineData(int.MaxValue, 2, int.MaxValue, 2)]
+		[InlineData(int.MaxValue, 2, 1, 2)]
+		[InlineData(1, 2, int.MaxValue, 2)]
+		public void Rational_OverflowSum_OverflowException(int r1Num, int r1Denom, int r2Num, int r2Denom)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+			Assert.Throws<OverflowException>(() => rational1 + rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, "0")]
+		[InlineData(1, 2, 2, 3, "-1/6")]
+		public void Rational_Substract(int r1Num, int r1Denom, int r2Num, int r2Denom, string stringResult)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			var result = rational1 - rational2;
+
+			Assert.Equal(stringResult, result.ToString());
+		}
+
+		[Theory]
+		[InlineData(int.MinValue, 2, int.MaxValue, 2)]
+		[InlineData(int.MinValue, 2, 1, 2)]
+		public void Rational_OverflowSubstract_OverflowException(int r1Num, int r1Denom, int r2Num, int r2Denom)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+			Assert.Throws<OverflowException>(() => rational1 - rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, "-1/2")]
+		[InlineData(-3, 2, "3/2")]
+		[InlineData(-4, 2, "2")]
+		[InlineData(int.MaxValue, 2, "-2147483647/2")]
+		public void Rational_InverseSum(int r1Num, int r1Denom, string stringResult)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+
+			var result = -rational1;
+
+			Assert.Equal(stringResult, result.ToString());
+		}
+
+		[Theory]
+		[InlineData(int.MinValue, 2)]
+		public void Rational_OverflowInverseSum_OverflowException(int r1Num, int r1Denom)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			Assert.Throws<OverflowException>(() => -rational1);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, "1/4")]
+		[InlineData(1, 2, 2, 3, "1/3")]
+		public void Rational_Multiply(int r1Num, int r1Denom, int r2Num, int r2Denom, string stringResult)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			var result = rational1 * rational2;
+
+			Assert.Equal(stringResult, result.ToString());
+		}
+
+		[Theory]
+		[InlineData(int.MaxValue, 2, int.MaxValue, 2)]
+		[InlineData(int.MinValue, 2, int.MinValue, 2)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue)]
+		[InlineData(int.MinValue, int.MinValue, int.MinValue, int.MinValue)]
+		public void Rational_OverflowMultiply_OverflowException(int r1Num, int r1Denom, int r2Num, int r2Denom)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+			Assert.Throws<OverflowException>(() => rational1 * rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, "1")]
+		[InlineData(1, 2, 1, 4, "2")]
+		public void Rational_Divide(int r1Num, int r1Denom, int r2Num, int r2Denom, string stringResult)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			var result = rational1 / rational2;
+
+			Assert.Equal(stringResult, result.ToString());
+		}
+
+		[Theory]
+		[InlineData(int.MaxValue, 2, int.MaxValue, 2)]
+		[InlineData(int.MinValue, 2, int.MinValue, 2)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue)]
+		[InlineData(int.MinValue, int.MinValue, int.MinValue, int.MinValue)]
+		public void Rational_OverflowDivide_OverflowException(int r1Num, int r1Denom, int r2Num, int r2Denom)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+			Assert.Throws<OverflowException>(() => rational1 / rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, true)]
+		[InlineData(2, 4, 1, 2, true)]
+		[InlineData(4, 2, 2, 1, true)]
+		[InlineData(0, 1, 0, 40, true)]
+		[InlineData(int.MinValue, int.MinValue, int.MinValue, int.MinValue, true)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, true)]
+		[InlineData(int.MinValue, int.MinValue, int.MaxValue, int.MaxValue, true)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue, true)]
+		[InlineData(int.MinValue, int.MaxValue, int.MinValue, int.MaxValue, true)]
+		[InlineData(int.MaxValue, int.MinValue, int.MaxValue, int.MinValue, true)]
+		[InlineData(1, 2, 1, 3, false)]
+		[InlineData(-2, 4, 1, 2, false)]
+		[InlineData(4, 2, -2, 1, false)]
+		[InlineData(int.MaxValue, int.MinValue, int.MinValue, int.MaxValue, false)]
+		[InlineData(int.MinValue, int.MaxValue, int.MaxValue, int.MinValue, false)]
+
+		[InlineData(679277, 679279, 679277, 679279, true)]
+		[InlineData(679279, 679277, 679277, 679279, false)]
+		[InlineData(679279, 679277, 679297, 679277, false)]
+		[InlineData(679297, 679277, 679279, 679277, false)]
+
+		[InlineData(1, -2, -2, 1, false)]
+		public void Rational_comparison_equal(int r1Num, int r1Denom, int r2Num, int r2Denom, bool expected)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			Assert.Equal(expected, rational1 == rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, false)]
+		[InlineData(2, 4, 1, 2, false)]
+		[InlineData(4, 2, 2, 1, false)]
+		[InlineData(0, 1, 0, 40, false)]
+		[InlineData(int.MinValue, int.MinValue, int.MinValue, int.MinValue, false)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, false)]
+		[InlineData(int.MinValue, int.MinValue, int.MaxValue, int.MaxValue, false)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue, false)]
+		[InlineData(int.MinValue, int.MaxValue, int.MinValue, int.MaxValue, false)]
+		[InlineData(int.MaxValue, int.MinValue, int.MaxValue, int.MinValue, false)]
+		[InlineData(1, 2, 1, 3, true)]
+		[InlineData(-2, 4, 1, 2, true)]
+		[InlineData(4, 2, -2, 1, true)]
+		[InlineData(int.MaxValue, int.MinValue, int.MinValue, int.MaxValue, true)]
+		[InlineData(int.MinValue, int.MaxValue, int.MaxValue, int.MinValue, true)]
+
+		[InlineData(679277, 679279, 679277, 679279, false)]
+		[InlineData(679279, 679277, 679277, 679279, true)]
+		[InlineData(679279, 679277, 679297, 679277, true)]
+		[InlineData(679297, 679277, 679279, 679277, true)]
+
+		[InlineData(1, -2, -2, 1, true)]
+		public void Rational_comparison_notequal(int r1Num, int r1Denom, int r2Num, int r2Denom, bool expected)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			Assert.Equal(expected, rational1 != rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, false)]
+		[InlineData(2, 4, 1, 2, false)]
+		[InlineData(4, 2, 2, 1, false)]
+		[InlineData(0, 1, 0, 40, false)]
+		[InlineData(int.MinValue, int.MinValue, int.MinValue, int.MinValue, false)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, false)]
+		[InlineData(int.MinValue, int.MinValue, int.MaxValue, int.MaxValue, false)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue, false)]
+		[InlineData(int.MinValue, int.MaxValue, int.MinValue, int.MaxValue, false)]
+		[InlineData(int.MaxValue, int.MinValue, int.MaxValue, int.MinValue, false)]
+		[InlineData(1, 2, 1, 3, false)]
+		[InlineData(-2, 4, 1, 2, true)]
+		[InlineData(4, 2, -2, 1, false)]
+		[InlineData(int.MaxValue, int.MinValue, int.MinValue, int.MaxValue, false)]
+		[InlineData(int.MinValue, int.MaxValue, int.MaxValue, int.MinValue, true)]
+
+		[InlineData(679277, 679279, 679277, 679279, false)]
+		[InlineData(679279, 679277, 679277, 679279, false)]
+		[InlineData(679279, 679277, 679297, 679277, true)]
+		[InlineData(679297, 679277, 679279, 679277, false)]
+
+		[InlineData(1, -2, -2, 1, false)]
+		public void Rational_comparison_less(int r1Num, int r1Denom, int r2Num, int r2Denom, bool expected)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			Assert.Equal(expected, rational1 < rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, true)]
+		[InlineData(2, 4, 1, 2, true)]
+		[InlineData(4, 2, 2, 1, true)]
+		[InlineData(0, 1, 0, 40, true)]
+		[InlineData(int.MinValue, int.MinValue, int.MinValue, int.MinValue, true)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, true)]
+		[InlineData(int.MinValue, int.MinValue, int.MaxValue, int.MaxValue, true)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue, true)]
+		[InlineData(int.MinValue, int.MaxValue, int.MinValue, int.MaxValue, true)]
+		[InlineData(int.MaxValue, int.MinValue, int.MaxValue, int.MinValue, true)]
+		[InlineData(1, 2, 1, 3, false)]
+		[InlineData(-2, 4, 1, 2, true)]
+		[InlineData(4, 2, -2, 1, false)]
+		[InlineData(int.MaxValue, int.MinValue, int.MinValue, int.MaxValue, false)]
+		[InlineData(int.MinValue, int.MaxValue, int.MaxValue, int.MinValue, true)]
+
+		[InlineData(679277, 679279, 679277, 679279, true)]
+		[InlineData(679279, 679277, 679277, 679279, false)]
+		[InlineData(679279, 679277, 679297, 679277, true)]
+		[InlineData(679297, 679277, 679279, 679277, false)]
+
+		[InlineData(1, -2, -2, 1, false)]
+		[InlineData(-1, 2, 2, -1, false)]
+		public void Rational_comparison_lessOrEqual(int r1Num, int r1Denom, int r2Num, int r2Denom, bool expected)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			Assert.Equal(expected, rational1 <= rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, false)]
+		[InlineData(2, 4, 1, 2, false)]
+		[InlineData(4, 2, 2, 1, false)]
+		[InlineData(0, 1, 0, 40, false)]
+		[InlineData(int.MinValue, int.MinValue, int.MinValue, int.MinValue, false)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, false)]
+		[InlineData(int.MinValue, int.MinValue, int.MaxValue, int.MaxValue, false)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue, false)]
+		[InlineData(int.MinValue, int.MaxValue, int.MinValue, int.MaxValue, false)]
+		[InlineData(int.MaxValue, int.MinValue, int.MaxValue, int.MinValue, false)]
+		[InlineData(1, 2, 1, 3, true)]
+		[InlineData(-2, 4, 1, 2, false)]
+		[InlineData(4, 2, -2, 1, true)]
+		[InlineData(int.MaxValue, int.MinValue, int.MinValue, int.MaxValue, true)]
+		[InlineData(int.MinValue, int.MaxValue, int.MaxValue, int.MinValue, false)]
+
+		[InlineData(679277, 679279, 679277, 679279, false)]
+		[InlineData(679279, 679277, 679277, 679279, true)]
+		[InlineData(679279, 679277, 679297, 679277, false)]
+		[InlineData(679297, 679277, 679279, 679277, true)]
+
+		[InlineData(1, -2, -2, 1, true)]
+		public void Rational_comparison_greater(int r1Num, int r1Denom, int r2Num, int r2Denom, bool expected)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			Assert.Equal(expected, rational1 > rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 1, 2, true)]
+		[InlineData(2, 4, 1, 2, true)]
+		[InlineData(4, 2, 2, 1, true)]
+		[InlineData(0, 1, 0, 40, true)]
+		[InlineData(int.MinValue, int.MinValue, int.MinValue, int.MinValue, true)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, true)]
+		[InlineData(int.MinValue, int.MinValue, int.MaxValue, int.MaxValue, true)]
+		[InlineData(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue, true)]
+		[InlineData(int.MinValue, int.MaxValue, int.MinValue, int.MaxValue, true)]
+		[InlineData(int.MaxValue, int.MinValue, int.MaxValue, int.MinValue, true)]
+		[InlineData(1, 2, 1, 3, true)]
+		[InlineData(-2, 4, 1, 2, false)]
+		[InlineData(4, 2, -2, 1, true)]
+		[InlineData(int.MaxValue, int.MinValue, int.MinValue, int.MaxValue, true)]
+		[InlineData(int.MinValue, int.MaxValue, int.MaxValue, int.MinValue, false)]
+
+		[InlineData(679277, 679279, 679277, 679279, true)]
+		[InlineData(679279, 679277, 679277, 679279, true)]
+		[InlineData(679279, 679277, 679297, 679277, false)]
+		[InlineData(679297, 679277, 679279, 679277, true)]
+
+		[InlineData(1, -2, -2, 1, true)]
+		public void Rational_comparison_greaterOrEqual(int r1Num, int r1Denom, int r2Num, int r2Denom, bool expected)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+			var rational2 = new Rational(r2Num, r2Denom);
+
+			Assert.Equal(expected, rational1 >= rational2);
+		}
+
+		[Theory]
+		[InlineData(1, 2, 0.5)]
+		[InlineData(1, 3, ((double)1 / 3))]
+		[InlineData(-1, 2, -0.5)]
+		[InlineData(1, -3, ((double)-1 / 3))]
+		[InlineData(0, 40, 0.0)]
+		[InlineData(2, 3, ((double)2 / 3))]
+		public void Rational_explicit_to_double(int r1Num, int r1Denom, double expected)
+		{
+			var rational1 = new Rational(r1Num, r1Denom);
+
+			Assert.Equal(expected, (double)rational1);
+		}
+
+		[Theory]
+		[InlineData(10, 10, 1)]
+		[InlineData(-10, -10, 1)]
+		[InlineData(0, 0, 1)]
+		public void Rational_implicit_from_int(int denominator, int r1NumExpected, int r1DenomExpected)
+		{
+			Rational rational = denominator;
+
+			Assert.Equal(r1NumExpected, rational.Numerator);
+			Assert.Equal(r1DenomExpected, rational.Denominator);
+		}
+
+		[Fact]
+		public void Rational_sorting()
+		{
+			var random = new Random();
+			var rationals = Enumerable.Range(1, 50).Select(val => new Rational((random.Next(0, 2) * 2 - 1) * val, (random.Next(0, 2) * 2 - 1) * val ^ val + 1)).ToList();
+
+			rationals.Sort();
+			var isOrdered =
+				rationals.Zip(rationals.Skip(1), (previous, current) => new { current, previous })
+					.All(p => (double)p.previous < (double)p.current);
+
+			Assert.True(isOrdered);
+		}
+	}
+}
